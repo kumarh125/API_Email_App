1e4c570b10bdf1e5a26eaba1be8435b0d6aa78b2
kumarh125 <hemantkumar3089@gmail.com>
2025-09-29 21:47:35 +0200

    Day 28: Add links, specify language, limit news, add subject, control category

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
 import requests
+from send_email import send_email
+import smtplib, ssl
 
 api = "1d0bda63cd0b42919489ac78c98ba423"
 
-url = "https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=1d0bda63cd0b42919489ac78c98ba423"
+topic = "tesla"
+
+url = "https://newsapi.org/v2/everything?" \
+      f"q={topic}&" \
+      "from=2025-08-29&" \
+      "sortBy=publishedAt&" \
+      "apiKey=1d0bda63cd0b42919489ac78c98ba423&" \
+      "language=en"    #go to api url properties and under api documents find endpoints
 
 #make a request
 request = requests.get(url)
@@ -11,10 +20,18 @@ request = requests.get(url)
 content = request.json()        #content = request.txt use json() for more readable format
 
 #Access the article titles and authors
+body = ""
+for article in content["articles"][:4]:
+    if article["title"] is not None:
+        body = "Subject: Todays news" + "\n" \
+               + body + article["title"] + "\n" \
+               + article["description"] + "\n" \
+               + article["url"] + 2*"\n"
+body = body.encode('utf-8')
+send_email(message = body)
+    #print(article["author"])
+    #print(article["title"])
 
-for article in content["articles"]:
-    print(article["author"])
-    print(article["title"])
 
 
 print(type(content))
